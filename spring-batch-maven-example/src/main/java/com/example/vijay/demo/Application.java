0365a3e3cc096e09cc8ae12c4c2f00b22fdbb1a0
One Social Inbox <onesocialinbox@gmail.com>
2015-04-24 17:54:44 +0800

    Removing target files. Spring Batch Template example.

---
+++ spring-batch-maven-example/src/main/java/com/example/vijay/demo/Application.java
@@ -0,0 +1,40 @@
+package com.example.vijay.demo;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException, InterruptedException {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring-batch-core.xml", "spring-batch-database.xml", "batch-job.xml");
+		
+		JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
+		
+		Job job = context.getBean("pricingJob", Job.class);
+		JobParameters jobParameters = new JobParameters();
+		JobExecution jobExecution = jobLauncher.run(job, jobParameters );
+		System.out.println("Job status  :::::: " + jobExecution.getStatus());
+		
+		BatchStatus batchStatus = jobExecution.getStatus();
+		
+		while (batchStatus.isRunning()) {
+			Thread.sleep(1000);
+			batchStatus = jobExecution.getStatus();
+		}
+		
+		System.out.println("Exit Status:::: " + batchStatus.getBatchStatus().toString());
+
+	}
+
+}
---
+++ spring-batch-maven-example/src/main/java/com/example/vijay/demo/SimpleTasklet.java
@@ -0,0 +1,21 @@
+package com.example.vijay.demo;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class SimpleTasklet implements Tasklet {
+
+	public SimpleTasklet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RepeatStatus execute(StepContribution contribution,
+			ChunkContext chunkContext) throws Exception {
+
+		System.out.println("Hello..........");
+		return RepeatStatus.FINISHED;
+	}
+
+}
